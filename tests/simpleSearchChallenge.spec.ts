346a85dc437c61cc0487c48025f902796dbac78d
Cesar Barragan <cesar.bh87@gmail.com>
2025-12-03 14:04:31 -0600

    Completed Simple Search Engine Challenge

--- tests/simpleSearchChallenge.spec.ts
+++ tests/simpleSearchChallenge.spec.ts
@@ -39,3 +39,76 @@ test("SSE_001 - Perform a valid search and verify Results ", async ({page})=>{
     }
 
 })
+
+test("SSE_002 - Attempt to search with empty input", async ({page})=>{
+/**
+ *  Ensure the search input is empty
+    Click the search button
+    Verify results section remains empty
+ */
+
+    const pm = new PageManager(page)
+    const searchField = page.getByRole("textbox")
+    expect(await searchField.inputValue()).toBe("")
+    
+       
+   // await pm.searchEnginePage().fillSearchBox("")
+    await pm.searchEnginePage().clickSearchButton()
+
+    const resultsSection = await pm.searchEnginePage().getResults()
+    expect(await resultsSection.first().isVisible()).toBeFalsy()
+    console.log(await resultsSection.count())
+
+
+})
+
+test("SSE_003 - Re-use old search input after re-render (Stale Element Exception)", async({page})=>{
+
+    /**
+    Locate the search input and enter 'Flights to London'
+    Click the search button
+    Wait for search results to load (input re-renders)
+    Attempt to type into the old input handle
+    Observe stale element exception
+    Recover by re-locating the search input and enter 'Hotels in Paris'
+    */
+
+
+    const pm = new PageManager(page)
+    await pm.searchEnginePage().fillSearchBox("Flight To London")
+    await pm.searchEnginePage().clickSearchButton()
+    const results = await pm.searchEnginePage().getResults()
+
+    await results.first().waitFor({state: 'visible'})
+
+    await pm.searchEnginePage().fillSearchBox('Flight to PARIS')
+
+})
+
+test("SSE_004 - Perform a second valid search after recovery", async({page})=>{
+    /**
+     * Re-locate the search input
+        Enter 'Node.js tutorials'
+        Click the search button
+        Verify that new results render correctly
+     */
+    const pm = new PageManager(page)
+    await pm.searchEnginePage().fillSearchBox("Node.js tutorials")
+    await pm.searchEnginePage().clickSearchButton()
+    const results = await pm.searchEnginePage().getResults()
+    await results.first().waitFor({state: 'visible'})
+
+    for(let i =0; i<await results.count();i++){
+        const title = results.nth(i).getByRole('link')
+        console.log(await title.textContent())
+        expect(await title.textContent()).toContain("Node.js")
+    }
+
+    //page.close()
+
+})
+
+
+test.afterAll(async ({page}) => {
+  await page.close(); // Close browser after all tests
+});
